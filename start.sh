9beb1975b199bda6af695bbafd7d5d839c085ef0
Mike-Chandlerr <schakal1.mc@gmail.com>
2026-06-07 19:27:27 +0200

    Update start.sh to change working directory to /app

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-cd /workspace
-python3 /workspace/download_models.py
+
+# In den App-Ordner wechseln, wo ComfyUI liegt
+cd /app
+
+# 1. Modell-Download starten (Modelle werden nach /workspace geladen, damit sie persistent bleiben!)
+python3 /app/download_models.py
+
+# 2. ComfyUI starten
 python3 main.py --listen 0.0.0.0 --port 8188 --highvram --fp8_e4m3fn-text-enc
