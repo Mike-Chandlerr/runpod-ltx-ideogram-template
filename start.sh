924652f2ca6b5a4c460b1caf484a771671344964
Mike-Chandlerr <schakal1.mc@gmail.com>
2026-06-07 19:32:08 +0200

    Update start.sh to launch Jupyter Lab in background

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
-# In den App-Ordner wechseln, wo ComfyUI liegt
 cd /app
 
-# 1. Modell-Download starten (Modelle werden nach /workspace geladen, damit sie persistent bleiben!)
+# 1. Modell-Download starten
 python3 /app/download_models.py
 
-# 2. ComfyUI starten
+# 2. Jupyter Lab im Hintergrund starten (ohne Passwort für schnellen RunPod-Connect)
+jupyter lab --allow-root --ip=0.0.0.0 --port=8888 --no-browser --NotebookApp.token='' --NotebookApp.password='' &
+
+# 3. ComfyUI im Vordergrund starten
 python3 main.py --listen 0.0.0.0 --port 8188 --highvram --fp8_e4m3fn-text-enc
